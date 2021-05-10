e02ab2a4833a14d557096e5fbde9acc36453666c
RealMrCactus <36554881+Bhraedain@users.noreply.github.com>
2021-05-09 20:34:32 -0400

    Update answerList.js

--- answerList.js
+++ answerList.js
@@ -1,15 +1,5 @@
 const answersList = [
   "I lost my job at the bank on my very first day. – A woman asked me to check her balance, so I pushed her over.",
   "What did the football coach say when he went to the bank? -“I want my quarter back.”",
-  "A man with a gun goes into a bank and demands for money.
-
-Once he is given the money, he turns to a customer and asks, “Did you see me rob this bank?”
-
-The man replied, “Yes sir, I did.”
-
-The robber shot him in the head, killing him instantly.
-
-He then turned to a couple standing next to him and asked the man, “Did you see me rob this bank?”
-
-The man replied, “No sir, I didn’t, but my wife did!”"
+  "A man with a gun goes into a bank and demands for money. Once he is given the money, he turns to a customer and asks, “Did you see me rob this bank?” The man replied, “Yes sir, I did.” The robber shot him in the head, killing him instantly. He then turned to a couple standing next to him and asked the man, “Did you see me rob this bank?” The man replied, “No sir, I didn’t, but my wife did!”"
 ]
